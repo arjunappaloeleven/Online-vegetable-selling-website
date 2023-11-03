0a11a12b966444ec77be4881d6dc6622bdac644f
arjunappaloeleven <arjuna.21it@kongu.edu>
2023-11-03 10:24:25 +0530

    first commit

---
+++ src/homeproduct.js
@@ -0,0 +1,60 @@
+const Homeproduct = 
+[
+    {
+        id:1,
+        Title:"Fresho Ginger - Organically Grown, 500 g",
+        Cat: 'Ginger',
+        Price: '83.22',
+        Img: './img/tp1.jpg'
+    },
+    {
+        id:2,
+        Title:"Fresh Cucumber - English (Loose), 500 g",
+        Cat: 'Cucumber',
+        Price: '168',
+        Img: './img/tp2.jpg'
+    },
+    {
+        id:3,
+        Title:"Fresh Pear - Green, Imported, 2x4 pcs Multipack",
+        Cat: 'Pear',
+        Price: '49',
+        Img: './img/tp3.jpg'
+    },
+    {
+        id:4,
+        Title:"Fresh Amaranthus - Red Cleaned, without roots, 250 g",
+        Cat: ' Amaranthus',
+        Price: '22.5',
+        Img: './img/tp4.jpg'
+    },
+    {
+        id:5,
+        Title:"Fresh Carrot - Orange (Loose), 250 g.",
+        Cat: 'Carrot',
+        Price: '12',
+        Img: './img/tp5.jpg'
+    },
+    {
+        id:6,
+        Title:"Fresh Cauliflower, 1 pc (approx. 400 to 600 g)",
+        Cat: 'Cauliflower',
+        Price: '27.3',
+        Img: './img/tp6.png'
+    },
+    {
+        id:7,
+        Title:"Fresh Onion - Organically Grown (Loose), 5 kg",
+        Cat: 'Onion',
+        Price: '196',
+        Img: './img/tp7.png'
+    },
+    {
+        id:8,
+        Title:"Fresh Corriander - Organically Grown, 1 kg",
+        Cat: 'Corriander',
+        Price: '386',
+        Img: './img/tp8.jpg'
+    },
+]
+export default Homeproduct
---
+++ src/productdetail.js
@@ -0,0 +1,172 @@
+const Productdetail =
+[
+    {
+        id:1,
+        Title:"Fresh Farm Eggs - Table Tray, Medium, Antibiotic Residue-Free, 30 pcs",
+        Cat: 'Eggs',
+        Price: '239',
+        Img: './img/a1.jpg'
+    },
+    {
+        id:2,
+        Title:"Fresh Mushrooms - Button, 1 pack (Approx. 180g - 200 g).",
+        Cat: 'Mushrooms',
+        Price: '45',
+        Img: './img/a2.jpg'
+    },
+    {
+        id:3,
+        Title:"Fresh Curry Leaves, 1 kg",
+        Cat: 'Curry Leaves',
+        Price: '70',
+        Img: './img/a3.jpg'
+    },
+    {
+        id:4,
+        Title:"Fresh Drumstick/Moringa, 1 kg",
+        Cat: 'Drumstick',
+        Price: '101',
+        Img: './img/a4.jpg'
+    },
+    {
+        id:5,
+        Title:"Fresh Chow Chow (Loose), 1 kg",
+        Cat: 'Chow Chow',
+        Price: '49',
+        Img: './img/a5.jpg'
+    },
+    {
+        id:6,
+        Title:"Fresh Coconut - Organically Grown (Loose), 1 pc (approx. 300g to 400g)",
+        Cat: 'Coconut',
+        Price: '35',
+        Img: './img/p9.jpg'
+    },
+    {
+        id:7,
+        Title:"Fresh Capsicum - Green (Loose), 500 g",
+        Cat: 'Capsicum',
+        Price: '22',
+        Img: './img/tp7.jpg'
+    },
+    {
+        id:8,
+        Title:"Fresh Beans - Broad (Loose), 1 kg",
+        Cat: 'Beans',
+        Price: '97',
+        Img: './img/tp8.jpg'
+    },
+    {
+        id:9,
+        Title:"Fresh Pomegranate - Regular (Loose), 1 kg (5-6 pcs per kg)",
+        Cat: 'Pomegranate',
+        Price: '199.5',
+        Img: './img/a6.jpg'
+    },
+    {
+        id:10,
+        Title:"Fresh Custard Apple - Organically Grown, 4 pcs (Approx 600-700gms)",
+        Cat: 'Custard Apple',
+        Price: '33.7',
+        Img: './img/p10.jpg'
+    },
+    {
+        id:11,
+        Title:"Fresh Papaya Medium - Organically Grown, 1 pc 1 kg - 1.5 kg",
+        Cat: 'Papaya Medium ',
+        Price: '104',
+        Img: './img/p11.jpg'
+    },
+    {
+        id:12,
+        Title:"Fresh Strawberry - Organically Grown, 200 g",
+        Cat: 'Strawberry',
+        Price: '64.24',
+        Img: './img/p12.jpg'
+    },
+    {
+        id:13,
+        Title:"Fresh Baby Apple Shimla, 1 kg (Approx. 11-12 pcs)",
+        Cat: 'Apple',
+        Price: '134',
+        Img: './img/a7.jpg'
+    },
+    {
+        id:14,
+        Title:"Fresh Mini Orange Imported (Loose), 1 kg",
+        Cat: 'Orange',
+        Price: '268',
+        Img: './img/a8.jpg'
+    },
+    {
+        id:15,
+        Title:"Fresh Blueberry, 125 g",
+        Cat: 'BlueBerry',
+        Price: '283',
+        Img: './img/a9.jpg'
+    },
+    {
+        id:16,
+        Title:"Fresh Grapes - Bangalore Blue, 1 kg",
+        Cat: 'Grapes',
+        Price: '106.8',
+        Img: './img/a10.jpg'
+    },
+    {
+        id:17,
+        Title:"Fresh Mosambi - Organically Grown (Loose), 1 kg",
+        Cat: 'Orange',
+        Price: '79.57',
+        Img: './img/a11.jpg'
+    },
+    {
+        id:18,
+        Title:"Fresh Potato, 1 kg",
+        Cat: 'Potato',
+        Price: '80',
+        Img: './img/a12.jpg'
+    },
+    {
+        id:19,
+        Title:"Fresh Apple - Green, Regular, 4 pcs Approx(500g-650g)",
+        Cat: 'Apple',
+        Price: '80',
+        Img: './img/a13.jpg'
+    },
+    {
+        id:20,
+        Title:"Fresh Amla (Loose), 100 g",
+        Cat: 'Amla',
+        Price: '150',
+        Img: './img/a14.jpg'
+    },
+    {
+        id:21,
+        Title:"Fresh Garlic (Loose), 250 g",
+        Cat: 'Garlic',
+        Price: '200',
+        Img: './img/a15.jpg'
+    },
+    {
+        id:22,
+        Title:"Fresh Watermelon - Medium, 1 pc 2.5 - 3.8 kg",
+        Cat: 'Water Melon',
+        Price: '200',
+        Img: './img/a16.jpg'
+    },
+    {
+        id:23,
+        Title:"Fresh Dragon Fruit (Loose), 1 pc 250 to 350 g",
+        Cat: 'Dragon Fruit',
+        Price: '200',
+        Img: './img/a17.jpg'
+    },
+    {
+        id:24,
+        Title:"Sweet Potato , 3 pc 300 to 500 g",
+        Cat: 'Sweet Potato',
+        Price: '200',
+        Img: './img/a18.jpg'
+    },
+]
+export default Productdetail;
